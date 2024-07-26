f9c9da88e073e50eb30ce26d004ce8e804fe7c87
Milad Raeisi <miladsoft@yahoo.com>
2024-07-26 12:16:12 +0330

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  base: '/astro-spa-tailwind/', //github repo name
-  integrations: [
-    react(),
-    tailwind()
-  ]
+  integrations: [react(), tailwind()],
+  site: 'https://astro-spa-tailwind.miladraeisi.com', 
 });
